1eee47b2dec6bb0bd26f7b274f4d2b6fa156a191
Brandon Smith Aranda Tacza <smith_aranda@hotmail.com>
2025-05-28 23:31:58 -0500

    Proyecto Corregido

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-urban_routes_url = 'https://cnt-e74a8307-79d2-43b0-b08b-35ba0f9fff99.containerhub.tripleten-services.com?lng=es'
+urban_routes_url = 'https://cnt-bd82bc5a-1bc3-4b46-8eca-90a9f6b0c893.containerhub.tripleten-services.com?lng=es'
 address_from = 'East 2nd Street, 601'
 address_to = '1300 1st St'
 card_tariff_name = 'Comfort'
